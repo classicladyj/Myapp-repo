ad1816b69e62bdc00730876a85ec812f974028a2
Judith Okwu <okwujudith@gmail.com>
2023-04-07 13:24:48 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors())
+
+const list_of_students=
+[
+  {
+    "name":"Judith",
+    "course":"Chemistry"
+  },
+  
+  {
+    "name":"Precious",
+    "course":"Computer "
+  },
+
+  {
+    "name":"Goodness",
+    "course":"Maths"
+  },
+
+  {
+    "name":"Ezekiel",
+    "course":"Physics"
+  },
+
+]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,14 +41,15 @@ app.get('/login', (req, res) => {
     res.send('Logged out!')
   })
 
-  app.get('/getTime', (req, res) => {
-    const currentDate = new Date();
-    const options = { hour12: true };
-    const currentTime = currentDate.toLocaleTimeString('en-US', options);
-    res.send(`The current time is: ${currentTime}`);
-  });
+  app.get('/list-of-students', (req, res) => {
+    res.send(list_of_students)
+    })
+
+    app.get('/random-student', (req, res) => {
+      let random_number=Math.floor(Math.random() * 4);
+      res.send(list_of_students[random_number])
+      })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
-
